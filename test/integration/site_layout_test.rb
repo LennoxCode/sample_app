a1b90c4b2f615c8a0d4010236b1edc1e8702b471
leonard.valentin1999@gmail.com <leonard.valentin1999@gmail.com>
2021-01-20 19:35:12 +0100

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:leonard)
+  end
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,8 +14,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
-    get signup_path 
+    get signup_path
     assert_select "title", full_title("Sign up")
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
 
   end
 end
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,103 @@
+require "test_helper"
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:leonard)
+    @other_user = users(:richard)
+  end
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "foo@invalid",
+                                              password: "foo",
+                                              password_confirmation: "bar" } }
+    assert_select "div.alert", text: "The form contains 4 errors"
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "different name"
+    email = "different@mail.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password:  "" ,
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "different name"
+    email = "different@mail.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not loggen in" do
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    name = "different name"
+    email = "different@mail.com"
+    originalName = @user.name
+    originalEmail = @user.email
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password:  "" ,
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+    @user.reload
+    assert_equal originalName, @user.name
+    assert_equal originalEmail, @user.email
+  end
+
+  test "should redirect edit when logged in as the wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as the wrong user" do
+    log_in_as(@other_user)
+    name = "different name"
+    email = "different@mail.com"
+    originalName = @user.name
+    originalEmail = @user.email
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password:  "" ,
+                                              password_confirmation: "" } }
+
+    assert_redirected_to root_url
+    assert flash.empty?
+    @user.reload
+    assert_equal originalName, @user.name
+    assert_equal originalEmail, @user.email
+  end
+end
